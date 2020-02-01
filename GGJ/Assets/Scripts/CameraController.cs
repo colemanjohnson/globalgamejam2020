0fc603edad1f30b2c1c6dd5ac39a6d54dcdead35
colemanjohnson <coleman.cjohnson@gmail.com>
2020-01-31 23:51:23 -0800

    camera scale interpolation

--- GGJ/Assets/Scripts/CameraController.cs
+++ GGJ/Assets/Scripts/CameraController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;        //Public variable to store a reference to the player game object
+    public PlayerControls player;        //Public variable to store a reference to the player game object
 
     // Use this for initialization
     void Start()
@@ -14,6 +14,8 @@ public class CameraController : MonoBehaviour
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
+        Camera.main.orthographicSize = Mathf.Lerp( Camera.main.orthographicSize, player.transform.localScale.x * 5, 0.05f );
+        //Camera.main.orthographicSize = player.transform.localScale.x * 5;
         transform.position = player.transform.position + new Vector3( 0, 0, transform.position.z );
     }
 }
