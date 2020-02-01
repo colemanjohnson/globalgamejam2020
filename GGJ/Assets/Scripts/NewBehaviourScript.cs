ee5b9778b035703978005a087c6d518e7f352477
colemanjohnson <coleman.cjohnson@gmail.com>
2020-01-31 20:21:42 -0800

    Demo script

---
+++ GGJ/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<BoxCollider>().transform.Rotate(new Vector3(1.0f, 0.0f, 0.0f));
+    }
+}
