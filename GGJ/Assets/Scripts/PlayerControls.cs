2c1f191107da9f45f121e077a04b09ac3f3652a5
Eric Roberts <ericjasonroberts@gmail.com>
2020-01-31 21:06:13 -0800

    Added basic snowman

---
+++ GGJ/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+	public float acceleration = 1;
+	public float maxSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	float hor = Input.GetAxis( "Horizontal" );
+    	// float ver = Input.GetAxis( "Vertical" );
+
+    	var body = GetComponent<Rigidbody2D>();
+    	if ( body.velocity.magnitude < maxSpeed )
+    		body.AddForce( new Vector2( hor * acceleration, 0 ) );
+    }
+}
