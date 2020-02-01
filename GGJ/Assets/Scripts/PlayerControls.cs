8d7640dec0c65857653ea2d56c435170380c1ca5
Eric Roberts <ericjasonroberts@gmail.com>
2020-01-31 22:18:16 -0800

    aded snow accumulation

--- GGJ/Assets/Scripts/PlayerControls.cs
+++ GGJ/Assets/Scripts/PlayerControls.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-	public float acceleration = 1;
-	public float maxSpeed = 10;
-	public float snowVolume = 1;
+	public float moveForce = 1.0f;
+	public float maxSpeed = 10.0f;
+	public float snowVolume = 1.0f;
+	public float minSnowVolume = 1.0f;
+	public float maxSnowVolume = 100.0f;
+	public float snowAccumulationFactor = 0.001f;
+
+	void SetVolume( float volume )
+	{
+		snowVolume = Mathf.Clamp( volume, minSnowVolume, maxSnowVolume );
+    	var transform = GetComponent<Transform>();
+
+    	float scale = Mathf.Sqrt( volume );
+    	transform.localScale = new Vector2( scale, scale );
+	}
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetVolume( snowVolume );
     }
 
     // Update is called once per frame
@@ -23,14 +35,16 @@ public class PlayerControls : MonoBehaviour
     	var collider = GetComponent<CircleCollider2D>();
     	var transform = GetComponent<Transform>();
 
-    	var other = Physics2D.OverlapCircle( new Vector2( transform.position.x, transform.position.y - 0.1f ), collider.radius, LayerMask.GetMask( "Terrain" ) );
+    	var other = Physics2D.OverlapCircle( new Vector2( transform.position.x, transform.position.y - 0.1f ), collider.radius * transform.localScale.x, LayerMask.GetMask( "Terrain" ) );
     	bool onGround = other != null;
 
     	if ( onGround )
     	{
 	    	var body = GetComponent<Rigidbody2D>();
 	    	if ( body.velocity.magnitude < maxSpeed )
-	    		body.AddForce( new Vector2( hor * acceleration, 0 ) );
+	    		body.AddForce( new Vector2( hor * moveForce, 0 ) );
+
+	    	SetVolume( snowVolume + Mathf.Abs( body.velocity.x ) * snowAccumulationFactor );
 	    }
     }
 }
