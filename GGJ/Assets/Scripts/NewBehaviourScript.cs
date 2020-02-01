ee78f335d23320ad32c454abed01dc19789fb8e3
colemanjohnson <coleman.cjohnson@gmail.com>
2020-01-31 20:30:11 -0800

    Project cleanup
    Level1 scene added

--- GGJ/Assets/Scripts/NewBehaviourScript.cs
+++ GGJ/Assets/Scripts/NewBehaviourScript.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
 
-    // Update is called once per frame
-    void Update()
-    {
-        GetComponent<BoxCollider>().transform.Rotate(new Vector3(1.0f, 0.0f, 0.0f));
-    }
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		GetComponent<BoxCollider>().transform.Rotate( new Vector3( 1.0f, 0.0f, 0.0f ) );
+	}
 }
