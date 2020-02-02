4b2a510c7ab1d698d6c8e02e8ca59901379a556d
colemanjohnson <coleman.cjohnson@gmail.com>
2020-02-02 14:11:25 -0800

    meteorite enemy

---
+++ GGJ/Assets/Scripts/FireballBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballBehavior : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float despawnTime = 10.0f;
+
+    private float initTime = 0.0f;
+
+    private void Start()
+    {
+        initTime = Time.timeSinceLevelLoad;
+    }
+    private void Update()
+    {
+        if ( Time.timeSinceLevelLoad - initTime >= despawnTime )
+        {
+            Destroy( gameObject );
+        }
+    }
+
+    public void FireAt( GameObject target )
+    {
+        var dir = target.transform.position - transform.position;
+        var angle = 90 + Mathf.Atan2( dir.y, dir.x ) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis( angle, Vector3.forward );
+        GetComponent<Rigidbody2D>().velocity = speed * dir.normalized;
+    }
+
+    private void OnCollisionEnter2D( Collision2D collision )
+    {
+        Destroy( gameObject );
+    }
+}
---
+++ GGJ/Assets/Scripts/MeteoriteBehavior.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteoriteBehavior : MonoBehaviour
+{
+    public Animator animator;
+    public FireballBehavior fireball;
+    public float activationRadius = 6.0f;
+    public float fireballShotDelay = 2.0f;
+    public float despawnAfterShotDelay = 0.5f;
+
+    private GameObject target = null;
+    private float delayRemaining = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<CircleCollider2D>().radius = activationRadius;
+        delayRemaining = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( delayRemaining > 0.0f )
+        {
+            delayRemaining -= Time.deltaTime;
+            if ( delayRemaining <= 0.0f )
+            {
+                // fire fireball
+                var newFireball = Instantiate( fireball );
+                newFireball.transform.position = transform.position;
+                newFireball.FireAt( target );
+                Destroy( gameObject, despawnAfterShotDelay );
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D( Collider2D collision )
+    {
+        if ( collision.tag == "Player" )
+        {
+            target = collision.gameObject;
+            animator.SetTrigger( "Activated" );
+            delayRemaining = fireballShotDelay;
+        }
+    }
+}
