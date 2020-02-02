89b1030d816476014496cbf5222629b8dca06847
Eric Roberts <ericjasonroberts@gmail.com>
2020-02-02 10:10:20 -0800

    Added finish condition

---
+++ GGJ/Assets/Scripts/Finish.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D( Collision2D collision )
+    {
+    	var other = collision.gameObject;
+    	
+    	if ( other.tag == "Player" )
+    	{
+    		var body = other.GetComponent<Rigidbody2D>();
+    		body.velocity = Vector2.zero;
+    		body.angularVelocity = 0;
+    		body.freezeRotation = true;
+    		body.isKinematic = true;
+    		body.simulated = false;
+    	}
+    }
+}
