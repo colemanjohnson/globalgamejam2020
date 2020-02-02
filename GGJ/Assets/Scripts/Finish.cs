44d4a75d2f7ddfd5031d70a82d0c08c1a2467a6a
Eric Roberts <ericjasonroberts@gmail.com>
2020-02-02 11:54:57 -0800

    set win parameters

--- GGJ/Assets/Scripts/Finish.cs
+++ GGJ/Assets/Scripts/Finish.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Finish : MonoBehaviour
 {
 	public GameObject sun;
+	public float minPlayerScale = 1;
+	public float maxPlayerScale = 100;
+	public float maxAngleDiffFromUp = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +21,20 @@ public class Finish : MonoBehaviour
         
     }
 
-    void OnCollisionEnter2D( Collision2D collision )
+    void OnCollision2D( Collision2D collision )
     {
     	var other = collision.gameObject;
     	
     	if ( other.tag == "Player" )
     	{
+    		float scale = other.transform.localScale.x;
+    		if ( scale < minPlayerScale || scale > maxPlayerScale )
+    			return;
+
+    		var toPlayer = other.transform.position - transform.position;
+    		if ( Vector3.Angle( new Vector3( 0, 1 ), toPlayer ) > maxAngleDiffFromUp )
+    			return;
+
     		var body = other.GetComponent<Rigidbody2D>();
     		body.velocity = Vector2.zero;
     		body.angularVelocity = 0;
@@ -34,4 +45,7 @@ public class Finish : MonoBehaviour
     		sun.GetComponent<Sun>().OnPlayerWin();
     	}
     }
+
+    void OnCollisionEnter2D( Collision2D c ) { OnCollision2D( c ); }
+    void OnCollisionStay2D( Collision2D c ) { OnCollision2D( c ); }
 }
