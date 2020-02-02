0b00c71d412c554e54e6a6afd1ddf27faa702a26
Eric Roberts <ericjasonroberts@gmail.com>
2020-02-01 19:48:44 -0800

    added sun charge state

--- GGJ/Assets/Scripts/Sun.cs
+++ GGJ/Assets/Scripts/Sun.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,26 +9,44 @@ public class Sun : MonoBehaviour
 	public GameObject target = null;
 	public float acceleration = 0.1f;
 	public float maxSpeed = 6;
-	public float stateTime = 5;
 	public float keepAwayDistance = 10.0f;
 	public float keepAwaySpeedFactor = 0.25f;
+	public float chargeSpeed = 8;
 
-	private bool attacking = false;
-	private float stateTimer = 0;
+	private enum State
+	{
+		KeepAway,
+		Attack,
+		Charge
+	}
+
+	private State state = State.KeepAway;
+	private float stateTimer = 5;
+
+	private Vector2 chargeDir;
 
     // Start is called before the first frame update
     void Start()
     {
-    	attacking = false;
+    	state = State.KeepAway;
     }
 
     void UpdateState()
     {
-    	stateTimer += Time.deltaTime;
-    	if ( stateTimer >= stateTime )
+    	stateTimer -= Time.deltaTime;
+    	if ( stateTimer < 0 )
     	{
-    		attacking = !attacking;
-    		stateTimer = 0;
+    		var values = Enum.GetValues( typeof( State ) );
+			var random = new System.Random();
+			state = (State)values.GetValue( random.Next( values.Length ) );
+
+			stateTimer = random.Next( 10 );
+
+			if ( state == State.Charge )
+			{
+			    var toTarget = ( target.transform.position - transform.position );
+			    chargeDir = toTarget.normalized;
+			}
     	}
     }
 
@@ -41,15 +60,27 @@ public class Sun : MonoBehaviour
 	    var toTarget = ( target.transform.position - transform.position );
 	    var dir = toTarget.normalized;
 
-        if ( attacking )
-        {
-	        body.velocity += (Vector2)dir * acceleration;
-        }
-        else
-        {
-        	float dist = toTarget.magnitude - keepAwayDistance;
-        	body.velocity += (Vector2)dir * dist * keepAwaySpeedFactor;
-        }
+	    switch( state )
+	    {
+	    	case State.KeepAway:
+	    	{
+	        	float dist = toTarget.magnitude - keepAwayDistance;
+	        	body.velocity += (Vector2)dir * dist * keepAwaySpeedFactor;
+	    		break;
+	    	}
+
+	    	case State.Attack:
+	    	{
+	     		body.velocity += (Vector2)dir * acceleration;
+	    		break;
+	    	}
+
+	    	case State.Charge:
+	    	{
+			    body.velocity = chargeDir * chargeSpeed;
+	    		break;
+	    	}
+	    }
 
         if ( body.velocity.magnitude > maxSpeed )
         {
